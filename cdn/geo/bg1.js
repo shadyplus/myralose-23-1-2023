f27acb372a32d1e8b3aafbfacad2643e1081fc0a
shadyplus <86576859+shadyplus@users.noreply.github.com>
2023-01-23 14:26:40 +0200

    first commit

---
+++ cdn/geo/bg1.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+
+    window.countryList = {
+        bg: {
+            name: 'الجزائر',
+            code: 'bg',
+            price: 69,
+            oldPrice: 138,
+            labelPrice: 'лева',
+            phoneHelper: 'Например, +359 123456789',
+            nameHelper: 'Jivko Djurovic',
+            nameError: 'Името е задължително',
+            phoneError: 'Телефонният номер е задължителен',
+            countryError: 'Страната е задължителна',
+            isDefault: true
+        }
+    };
+    
+})();
---
+++ index_files/all.js
@@ -0,0 +1,7 @@
+$('a').click(function(e){$("body,html").animate({scrollTop:$('form').offset().top},1e3);e.preventDefault();})
+setTimeout(function(){coments();},500);function coments(){var w=window.innerWidth;if(w>769){$('#comments_box').fadeIn();}
+var speed=10000;var run=setInterval(rotate,speed);var slides=$('.slide');var container=$('#slides ul');var elm=container.find(':first-child').prop("tagName");var item_width=container.width();var previous='prev';var next='next';slides.width(item_width);container.parent().width(item_width);container.width(slides.length*item_width);container.find(elm+':first').before(container.find(elm+':last'));resetSlides();$('#buttons a').click(function(e){if(container.is(':animated')){return false;}
+if(e.target.id==previous){container.stop().animate({'left':0},1500,function(){container.find(elm+':first').before(container.find(elm+':last'));resetSlides();});}
+if(e.target.id==next){container.stop().animate({'left':item_width*-2},1500,function(){container.find(elm+':last').after(container.find(elm+':first'));resetSlides();});}
+return false;});container.parent().mouseenter(function(){clearInterval(run);}).mouseleave(function(){run=setInterval(rotate,speed);});function resetSlides(){container.css({'left':-1*item_width});}}
+function rotate(){$('#next').click();}
